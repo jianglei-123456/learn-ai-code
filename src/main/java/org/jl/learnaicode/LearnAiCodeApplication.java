fa2b6388dadc097f49e97abcfa5b1c7988d68872
jiangl <jiangl@datian360.com>
2026-07-20 13:41:23 +0800

    init

---
+++ src/main/java/org/jl/learnaicode/LearnAiCodeApplication.java
@@ -0,0 +1,13 @@
+package org.jl.learnaicode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnAiCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnAiCodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/jl/learnaicode/LearnAiCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jl.learnaicode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnAiCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
